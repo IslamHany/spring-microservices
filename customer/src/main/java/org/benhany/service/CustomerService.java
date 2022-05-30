330dac76a7f8a93780d0952f5bd39640d5bb0a86
islam hany <islamhany@islams-MacBook-Air.local>
2022-05-30 19:58:48 +0200

    communicate between customer service and fraud using rest template

--- customer/src/main/java/org/benhany/service/CustomerService.java
+++ customer/src/main/java/org/benhany/service/CustomerService.java
@@ -1,10 +1,12 @@
 package org.benhany.service;
 
 import org.benhany.dto.CustomerRegistrationRequest;
+import org.benhany.dto.FraudCheckResponse;
 import org.benhany.model.Customer;
 import org.benhany.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.UUID;
 
@@ -14,14 +16,26 @@ public class CustomerService {
     @Autowired
     CustomerRepository customerRepository;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     public void registerCustomer(CustomerRegistrationRequest request){
+
         Customer customer = Customer.builder()
                 .firstName(request.getFirstName())
                 .lastName(request.getLastName())
                 .email(request.getEmail())
-//                .id(UUID.randomUUID())
                 .build();
 
-        customerRepository.save(customer);
+        customerRepository.saveAndFlush(customer);
+
+        FraudCheckResponse fraudCheckResponse = restTemplate.getForObject(
+                "http://localhost:8081/api/v1/fraud/{customerId}",
+                FraudCheckResponse.class,
+                customer.getId()
+        );
+
+        if (fraudCheckResponse.getIsFraudster()) throw new IllegalStateException("fraudster");
+
     }
 }
--- fraud/src/main/java/org/benhany/controller/FraudController.java
+++ fraud/src/main/java/org/benhany/controller/FraudController.java
@@ -1,10 +1,12 @@
 package org.benhany.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.benhany.dto.FraudCheckResponse;
 import org.benhany.service.FraudCheckService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 @RequestMapping("api/v1/fraud")
 public class FraudController {
@@ -15,6 +17,7 @@ public class FraudController {
     @GetMapping("/{customerId}")
     public FraudCheckResponse isFraudster(@PathVariable(name = "customerId") Integer customerId){
         Boolean isFraudulentCustomer = fraudCheckService.isFraudulent(customerId);
+        log.info("fraud check request for customer {}", customerId);
         return FraudCheckResponse.builder().isFraudster(isFraudulentCustomer).build();
     }
 }
