1e79ea0b1152a4a77f9a1a561d01ed09b091978c
islam hany <islamhany@islams-MacBook-Air.local>
2022-06-07 17:05:53 +0200

    Refactoring Microservices for k8s

--- clients/src/main/java/org/benhany/clients/fraud/FraudClient.java
+++ clients/src/main/java/org/benhany/clients/fraud/FraudClient.java
@@ -5,7 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient("fraud") //fraud is the client name in eureka
+@FeignClient(
+        name = "fraud",
+        url = "${clients.fraud.url}"
+) //fraud is the client name in eureka
 public interface FraudClient {
 
     @GetMapping("/api/v1/fraud/{customerId}")
--- clients/src/main/java/org/benhany/clients/notification/NotificationClient.java
+++ clients/src/main/java/org/benhany/clients/notification/NotificationClient.java
@@ -4,7 +4,10 @@ import org.benhany.clients.notification.dto.NotificationRequest;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name = "notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
 
     @PostMapping("/api/v1/notification")
